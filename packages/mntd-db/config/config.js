d01119f619662084273c17802716aa253471e328
kenriortega <kenriortega@gmail.com>
2021-05-01 08:35:57 -0400

    [ADD] no ssl

--- packages/mntd-db/config/config.js
+++ packages/mntd-db/config/config.js
@@ -18,6 +18,11 @@ module.exports = {
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false, // very important
+      }
+    }
   }
 }
